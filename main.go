c027402fc20225f7731dc5f15a8adad2e61a8721
CookieBorn <robanikin@hotmail.com>
2025-03-13 06:21:46 +1100

    C: Testing

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Printf("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	textStrip := strings.TrimSpace(text)
+	textStrip = strings.ToLower(textStrip)
+	result := strings.Split(textStrip, " ")
+	return result
 }
---
+++ repl_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  test best  ",
+			expected: []string{"test", "best"},
+		},
+		{
+			input:    "  Charmander Bulbasaur PIKACHU ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(c.expected) != len(actual) {
+			t.Errorf("Fail lengths incorrect: expected %v actual %v", len(c.expected), len(actual))
+		}
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Fail, words don't match: excpected %s actual %s", expectedWord, word)
+			}
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
+	}
+}

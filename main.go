2b31f1696a820cb986a910aa6d019e977f33a3ca
CookieBorn <robanikin@hotmail.com>
2025-03-14 06:35:00 +1100

    E: Exit and Help

--- main.go
+++ main.go
@@ -14,7 +14,17 @@ func main() {
 		scanner.Scan()
 		command := scanner.Text()
 		clean := cleanInput(command)
-		fmt.Printf("Your command was: %s\n", clean[0])
+		calledCommand := false
+		for com, cli := range callbackMap {
+			if com == clean[0] {
+				calledCommand = true
+				cli.callback()
+			}
+		}
+		if calledCommand == false {
+			fmt.Print("Unknown command\n")
+		}
+
 	}
 }
 
@@ -23,4 +33,59 @@ func cleanInput(text string) []string {
 	textStrip = strings.ToLower(textStrip)
 	result := strings.Split(textStrip, " ")
 	return result
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, cli := range callbackMap {
+		fmt.Printf("%s: %s\n", cli.name, cli.description)
+	}
+	return nil
+}
+
+func commandMap() error {
+	return nil
+}
+
+func commandMapB() error {
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var callbackMap map[string]cliCommand
+
+func init() {
+	callbackMap = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "show the next 20 locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "show the previous 20 locations",
+			callback:    commandMapB,
+		},
+	}
 }

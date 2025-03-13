1a4aeb5ac8d8a6cdcf89d1994009ac696d5a1335
CookieBorn <robanikin@hotmail.com>
2025-03-13 20:35:07 +1100

    D: Command line

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		command := scanner.Text()
+		clean := cleanInput(command)
+		fmt.Printf("Your command was: %s\n", clean[0])
+	}
 }
 
 func cleanInput(text string) []string {
